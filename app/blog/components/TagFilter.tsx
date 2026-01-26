fc09545ddb2323ec9e95f21dffd50b457946e401
Taka0330-jp <gtakanari0330@gmail.com>
2026-01-26 12:18:42 -0800

    feat: random picked tags render function

--- app/blog/components/TagFilter.tsx
+++ app/blog/components/TagFilter.tsx
@@ -1,7 +1,10 @@
 "use client";
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { PostCard } from '@/lib/posts'
 import { Button } from '@/components/ui/shadcn/button';
+import { arrayToShuffled } from 'array-shuffle';
+import { Card } from '@/components/ui/shadcn/card';
+import { cn } from '@/lib/utils';
 
 
 type Props = {
@@ -10,20 +13,72 @@ type Props = {
 
 export default function TagFilter({ tagInfo, }: Props) {
 
+    const [pickedTags, setPickedTags] = useState<string[]>([]);
+    const [unPickedTags, setUnPickedTags] = useState<string[]>([]);
+    const [isOpen, setIsOpen] = useState<boolean>(false);
 
-    const tags = Array.from(new Set(tagInfo.flatMap(post => post.tags ?? [])))
+    useEffect(() => {
+        const tags = Array.from(
+            new Set(tagInfo.flatMap((post) => post.tags ?? []))
+        );
+
+        const shuffled = arrayToShuffled(tags)
+
+        const pickedTags = shuffled.slice(0, 4);
+        const unPickedTags = shuffled.slice(4)
+
+        const timerId = window.setTimeout(() => {
+            setPickedTags(pickedTags);
+            setUnPickedTags(unPickedTags)
+        }, 0);
+
+        return () => {
+            window.clearTimeout(timerId);
+        };
+    }, [tagInfo]);
+
+    const handleClick = () => {
+        setIsOpen(prev => !prev)
+    }
 
 
 
     return (
-        <div className='flex flex-wrap' >
-            {tags.map((tag, index) => (
+        <div>
+            <div className='flex flex-wrap' >
+                {pickedTags.map((tag) => (
+                    <Button
+                        key={tag}
+                        className='bg-gray-100 text-black hover:text-white border mx-2 my-2 cursor-pointer' >{tag}</Button>
+                ))}
                 <Button
-                    key={index}
-
-                    className='bg-gray-100 text-black hover:text-white border mx-2 my-2 cursor-pointer' >{tag}</Button>
-            ))}
-
-        </div>
+                    onClick={handleClick}
+                    className={cn(
+                        'border mx-2 my-2 cursor-pointer',
+                        isOpen
+                            ? 'bg-black text-white'
+                            : 'bg-gray-100 text-black hover:text-white'
+                    )}
+                >
+                    More...</Button>
+            </div>
+            {
+                isOpen && (
+                    <Card className='rounded-lg bg-black border-gray-100 px-4 mx-2'>
+                        <div className='flex flex-wrap'>
+                            {unPickedTags.map((tag) => (
+                                <Button
+                                    key={tag}
+                                    className='bg-gray-100 text-black hover:text-white border mx-2 my-2 cursor-pointer' >{tag}</Button>
+                            ))}
+                        </div>
+                    </Card>
+                )
+            }
+        </div >
     )
 }
+
+//Reference:
+//https://www.npmjs.com/package/array-shuffle
+//https://coureywong.medium.com/how-to-shuffle-an-array-of-items-in-javascript-39b9efe4b567
