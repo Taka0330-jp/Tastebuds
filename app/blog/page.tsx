8d5fbf5cee6086793444479b207c968a2198b055
Taka0330-jp <gtakanari0330@gmail.com>
2026-01-27 21:01:28 -0800

    Chore: Change folders structure

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,19 +1,30 @@
 import React from 'react'
-import BlogHero from '@/app/blog/section/BlogHero'
-import BlogPostGrid from '@/app/blog/section/BlogPostGrid'
-
+import { DisplayH1 } from '@/components/typography/DisplayH1';
+import TagFilter from './components/TagFilter';
 import { getAllPosts } from '@/lib/posts'
+import CardM from '../../components/ui/CardM'
+
 
 export default async function page() {
 
     const allPosts = await getAllPosts();
     return (
         <main className='bg-blog-archive '>
-            <section className='max-w-container mx-auto pt-16'>
-                <BlogHero allPost={allPosts} />
+            {/* Blog-hero */}
+            <section className='blog-hero max-w-container mx-auto pt-16'>
+                <DisplayH1 className='text-text-inverse px-4'>Blog Post Archive</DisplayH1>
+
+                <TagFilter tagInfo={allPosts} />
             </section>
-            <section >
-                <BlogPostGrid allPosts={allPosts} />
+            {/* BlogCards */}
+            <section className='py-16'>
+                <div className='flex flex-col md:grid grid-cols-12 gap-x-10 gap-y-10 max-w-container mx-auto px-4'>
+                    {allPosts.map((post) => (
+                        <div className='md:col-span-6 lg:col-span-4' key={post.id}>
+                            <CardM posts={post} />
+                        </div>
+                    ))}
+                </div>
             </section>
         </main>
     )
