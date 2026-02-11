7d880c2df6fac4922ddce2a66b0742bfa40bec7c
Taka0330-jp <gtakanari0330@gmail.com>
2026-02-10 22:18:41 -0800

    get render popular posts

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import HomeHero from "@/components/sections/HomeHero";
 import Popular from "@/components/sections/Popular";
 import RecentPosts from "@/components/sections/RecentPosts";
+import { getAllPosts } from "@/lib/posts";
 import SubscribeForm from "@/components/sections/SubscribeForm";
 import { getRecentPosts } from "@/lib/posts";
 import PinnedAnimationSection from "@/components/sections/ScrollTrigger";
@@ -8,12 +9,13 @@ import PinnedAnimationSection from "@/components/sections/ScrollTrigger";
 
 export default async function Home() {
   const posts = await getRecentPosts(4);
+  const allPosts = await getAllPosts()
 
   return (
     <>
       <HomeHero />
       <RecentPosts posts={posts} />
-      <Popular />
+      <Popular posts={allPosts}/>
       <PinnedAnimationSection />
       <SubscribeForm />
     </>
--- components/sections/Popular.tsx
+++ components/sections/Popular.tsx
@@ -1,7 +1,17 @@
 import { DisplayH2 } from "../typography/DisplayH2";
 import Image from "next/image";
-import BlogCardM from "../ui/CardM";
-export default function Popular() {
+import CardM from "../ui/CardM";
+
+import { PostCard } from "@/lib/posts";
+
+type Props = {
+    posts: PostCard[];
+}
+
+export default function Popular({posts}: Props) {
+
+    console.log(posts);
+    
     return (
         <>
             <section className="bg-home-section relative z-10">
@@ -18,9 +28,9 @@ export default function Popular() {
                 </div>
                 <div className="md:grid md:grid-cols-12 gap-6 max-w-container mx-auto p-5  relative z-10">
                     <div className="md:col-span-12 grid gap-6 md:grid-cols-3">
-                        <BlogCardM />
-                        <BlogCardM />
-                        <BlogCardM />
+                    {posts[4] ? <CardM posts={posts[4]} /> : null}
+                    {posts[5] ? <CardM posts={posts[5]} /> : null}
+                    {posts[6] ? <CardM posts={posts[6]} /> : null}
                     </div>
                 </div>
             </section>
